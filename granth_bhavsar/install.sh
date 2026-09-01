dae64ff5bbb365df59c339a1dbf703475d4692c7
granth <granth.bhavsar@gmail.com>
2026-09-01 19:19:47 +0530

    Initial commit

---
+++ granth_bhavsar/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.config/wal/templates # makes sure required directories exist
+cp ./template/custom_palettes.toml $HOME/.config/wal/templates/ #copies template to required directory
+echo "Install Completed!" 
+
---
+++ granth_bhavsar/postrun.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp ~/.cache/wal/custom_palettes.toml ~/.config/pyroclear/custom_palettes.toml
+pyroclear --color pywal 
+
